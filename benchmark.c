66b4e5b8a55b11aff8ba81ae35ebc1db62a99224
fidoriel <49869342+fidoriel@users.noreply.github.com>
2025-02-10 11:00:09 +0100

    benchmark

---
+++ benchmark.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_ALLOCS 8092
+#define ALLOC_SIZE (1024 * 1024 * 128) // 1 MB
+
+int main() {
+    clock_t start, end;
+    double total_time;
+    
+    for (size_t i = 0; i < NUM_ALLOCS; i++)
+    {
+        start = clock();
+
+        void *mem = malloc(ALLOC_SIZE);
+        if (mem == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            return 1;
+        }
+        end = clock();
+        free(mem);
+        total_time += ((double) (end - start)) / CLOCKS_PER_SEC;
+    }
+
+    printf("Average allocation time: %.9f seconds\n", total_time / NUM_ALLOCS);
+
+    return 0;
+}
